3e6deb3975202b1f26d555c4fa27288e504e6140
SabrinaSimao <sabrina_simao@hotmail.com>
2020-03-09 19:18:04 -0300

    Minha Primeira Traducao

--- aulas/05-localizacao/cli.py
+++ aulas/05-localizacao/cli.py
@@ -1,13 +1,18 @@
-import babel
+from babel.numbers import *
+from babel.dates import *
 from datetime import date
 from datetime import date, datetime, time
+import gettext
+
+gettext.install('cli', localedir='locale')
+
 
 if __name__ == '__main__':
     today = date.today()
-    print(today)
+    print(format_date(today))
 
     number = 240000000000.32212
-    print(number)
+    print(format_number(number))
     
-    name = input('Input your name: ')
-    print('Hello {}'.format(name))
+    name = input(_('Input your name: '))
+    print(_('Hello {}').format(name))
