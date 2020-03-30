d39657c4859b304a8f8bce58f94d44332a3a05ec
Raphael Costa <rapha2706@hotmail.com>
2020-03-30 17:35:21 -0300

    Adicionando skills 4 e 6

--- aulas/05-localizacao/cli.py
+++ aulas/05-localizacao/cli.py
@@ -1,13 +1,20 @@
-import babel
+from babel import dates
+from babel import numbers
 from datetime import date
 from datetime import date, datetime, time
+import gettext
+
+gettext.install('cli', localedir='locale')
 
 if __name__ == '__main__':
     today = date.today()
-    print(today)
+    data = dates.format_datetime(today)
+    print(data)
 
     number = 240000000000.32212
+    number = numbers.format_number(number)
     print(number)
     
-    name = input('Input your name: ')
-    print('Hello {}'.format(name))
+    name = input(_('Input your name: '))
+    print(_('Hello {}').format(name))
+
