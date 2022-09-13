0083bd87b621bdfbe8e4ef1b253e77f4d57981c1
gustavomfb <gustavo.benites@onepartners.com.br>
2022-09-13 10:29:21 -0300

    Entrega em grupo - Docker - Skill 07

---
+++ aula07/cli.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+if __name__ == '__main__':
+    today = date.today()
+    print(today)
+
+    number = 240000000000.32212
+    print(number)
+
+    name = input('Input your name: ')
+    print('Hello {}'.format(name))
+
