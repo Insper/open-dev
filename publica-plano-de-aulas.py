1b58f004409d89210db3c0c56d03163bf23ccec2
Igor Montagner <igorsm1@insper.edu.br>
2023-07-15 10:47:52 -0300

    Translate lesson plan

--- publica-plano-de-aulas.py
+++ publica-plano-de-aulas.py
@@ -2,10 +2,10 @@ import tabulate
 import pandas as pd
 
 t1 = pd.read_excel('plano-de-aulas.xlsx')
-#t1['Data'] = t1['Data'].apply(lambda x: x.strftime('%d/%m'))
+t1['Date'] = t1['Date'].apply(lambda x: x.strftime('%d/%m'))
 
 with open('docs/_snippets/plano-de-aulas.md', 'w') as f:
-    tabela_str = tabulate.tabulate(t1[['Data', 'Questão/Problema/Desafio',
-'Conteúdo']], headers=['Data', 'Questão/Problema/Desafio',
-'Conteúdo'], tablefmt='pipe', showindex=False)
+    tabela_str = tabulate.tabulate(t1[['Date', 'Question/Problem/Challenge',
+'Content']], headers=['Date', 'Question/Problem/Challenge',
+'Content'], tablefmt='pipe', showindex=False)
     f.write(tabela_str)
