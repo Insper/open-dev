64c95a2a63df6ab200e972f6ecd4449c2a96b68c
JorasOliveira <joras.custodio@gmail.com>
2023-08-30 09:14:36 -0300

    entrega 6 feita

--- docs/lessons/06-localization-translation/cli.py
+++ docs/lessons/06-localization-translation/cli.py
@@ -1,12 +1,20 @@
 from datetime import date
-from gettext import _
+from babel.dates import format_date, format_datetime, format_time
+from babel.numbers import format_number, format_decimal
+import gettext
+gettext.bindtextdomain('cli', 'locale')
+gettext.textdomain('cli')
+_ = gettext.gettext
+# cli é o nome do arquivo em que guardamos nossas traduções
+# localedir é o caminho onde estão armazenadas as traduções. Pode ser um caminho relativo. 
 
 if __name__ == '__main__':
     today = date.today()
-    print(today)
+    print(format_date(today, format='short'))
 
     number = 240000000000.32212
-    print(number)
+    print(format_decimal(number))
     
     name = input(_('Input your name: '))
-    print('Hello {}'.format(name))
+    
+    print(_('Hello {}').format(name))
